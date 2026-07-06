076d7d8d29c4b215632a4bfaaceb6e4cbd5be68e
NawwalIbraheem <nawwalibraheem@gmail.com>
2026-07-06 23:06:38 +0300

    slash

--- apps/ussd/urls.py
+++ apps/ussd/urls.py
@@ -5,5 +5,6 @@ from .views import UssdMenuView
 app_name = "ussd"
 
 urlpatterns = [
+    path("menu", UssdMenuView.as_view(), name="menu-no-slash"),
     path("menu/", UssdMenuView.as_view(), name="menu"),
 ]
