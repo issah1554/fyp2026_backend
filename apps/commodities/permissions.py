2244f9430826b25c9cc5541137ad5ea358417362
issahg <nzikustephen826@gmail.com>
2026-07-29 04:56:04 +0300

    - - fix: allow public users to list and get commodities; update permission checks

--- apps/commodities/permissions.py
+++ apps/commodities/permissions.py
@@ -8,11 +8,12 @@ class IsAdminOrAuthenticatedReadOnly(PublicReadPermissionCode):
     message = "You do not have permission to manage commodities."
 
     def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
         user = request.user
         if not user or not user.is_authenticated:
             return False
-        if request.method in SAFE_METHODS:
-            return True
         if user.is_staff or user.is_superuser:
             return True
         try:
